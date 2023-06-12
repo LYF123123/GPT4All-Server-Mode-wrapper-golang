43f5addd7b40e5fd5023aff54905a4e11a24881b
lyf <luyifeng3@163.com>
2023-06-12 09:59:32 +0800

    add Models wrapper

--- wrapper.go
+++ wrapper.go
@@ -1,1 +1,18 @@
 package wrapper
+
+import "net/http"
+
+type GPTClient struct {
+	Client  *http.Client
+	ApiBase string //"http://localhost:4891/v1"
+	Token   string //"not needed for a local LLM"
+}
+
+func NewGPTClient(apiBase, token string) *GPTClient {
+	return &GPTClient{
+		Client:  &http.Client{},
+		ApiBase: apiBase,
+		Token:   token,
+	}
+}
+func (c *GPTClient) ListModels()
---
+++ wrapperStruct.go
@@ -0,0 +1,20 @@
+package wrapper
+
+type Models struct {
+	Data   []Datum `json:data`
+	Object string  `json:"object"`
+}
+
+type Datum struct {
+	Created     string       `json:"created"`
+	ID          string       `json:"id"`
+	Object      string       `json:"object"`
+	OwnedBy     string       `json:"owned_by"`
+	Parent      interface{}  `json:"parent"`
+	Permissions []Permission `json:"permissions"`
+	Root        string       `json:"root"`
+}
+
+type Permission struct {
+	//Todo
+}
