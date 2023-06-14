f310e06b84c1a809e80c1bb1005e12716336ec2b
lyf <luyifeng3@163.com>
2023-06-14 15:09:05 +0800

    implement edits and chat api. add comments and modify struct names

--- wrapper.go
+++ wrapper.go
@@ -3,7 +3,7 @@ package wrapper
 import (
 	"bytes"
 	"encoding/json"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"net/url"
 )
@@ -27,7 +27,7 @@ func NewGPTClient(apiBase, token string) (*GPTClient, error) {
 }
 
 // Models
-
+// apiBase/models
 func (c *GPTClient) ListModels() (Models, error) {
 	req, err := http.NewRequest("GET", c.ApiBase+"/models", nil)
 	if err != nil {
@@ -39,7 +39,7 @@ func (c *GPTClient) ListModels() (Models, error) {
 		return Models{}, err
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return Models{}, err
 	}
@@ -48,6 +48,7 @@ func (c *GPTClient) ListModels() (Models, error) {
 	return models, err
 }
 
+// apiBase/models/{modelId}
 func (c *GPTClient) RetrieveModel(modelId string) (Model, error) {
 	req, err := http.NewRequest("GET", c.ApiBase+"/models/"+modelId, nil)
 	if err != nil {
@@ -58,7 +59,7 @@ func (c *GPTClient) RetrieveModel(modelId string) (Model, error) {
 	if err != nil {
 		return Model{}, err
 	}
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return Model{}, err
 	}
@@ -67,11 +68,39 @@ func (c *GPTClient) RetrieveModel(modelId string) (Model, error) {
 	return model, err
 }
 
+// Chat
+// !Warning Not test
+// apiBase/chat/completions
+func (c *GPTClient) CreateChatCompletionRawRequest(cReq ChatReq) (ChatResp, error) {
+	reqBody, err := json.Marshal(cReq)
+	if err != nil {
+		return ChatResp{}, err
+	}
+	req, err := http.NewRequest("POST", c.ApiBase+"/completions", bytes.NewBuffer(reqBody))
+	if err != nil {
+		return ChatResp{}, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+c.Token)
+	resp, err := c.Client.Do(req)
+	if err != nil {
+		return ChatResp{}, err
+	}
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return ChatResp{}, err
+	}
+	cResp := ChatResp{}
+	err = json.Unmarshal(respBody, &cResp)
+	return cResp, err
+}
+
 // Completions
+// apiBase/completions
 func (c *GPTClient) CreateCompletion(prompt string) (CompletionResp, error) {
 	cReq := getDefaultDataCompletionReq()
 	if prompt != "" {
-		cReq.Prompt=prompt
+		cReq.Prompt = prompt
 	}
 	reqBody, err := json.Marshal(cReq)
 	if err != nil {
@@ -87,7 +116,7 @@ func (c *GPTClient) CreateCompletion(prompt string) (CompletionResp, error) {
 	if err != nil {
 		return CompletionResp{}, err
 	}
-	respBody, err := ioutil.ReadAll(resp.Body)
+	respBody, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return CompletionResp{}, err
 	}
@@ -96,7 +125,8 @@ func (c *GPTClient) CreateCompletion(prompt string) (CompletionResp, error) {
 	return cResp, err
 }
 
-func (c *GPTClient) CreateCompletionRawRequest(cReq CompletionReq)(CompletionResp, error){
+// apiBase/completions
+func (c *GPTClient) CreateCompletionRawRequest(cReq CompletionReq) (CompletionResp, error) {
 	reqBody, err := json.Marshal(cReq)
 	if err != nil {
 		return CompletionResp{}, err
@@ -111,11 +141,38 @@ func (c *GPTClient) CreateCompletionRawRequest(cReq CompletionReq)(CompletionRes
 	if err != nil {
 		return CompletionResp{}, err
 	}
-	respBody, err := ioutil.ReadAll(resp.Body)
+	respBody, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return CompletionResp{}, err
 	}
 	cResp := CompletionResp{}
 	err = json.Unmarshal(respBody, &cResp)
 	return cResp, err
+}
+
+// Edits
+// !Warning Not test
+// apiBase/edits
+func (c *GPTClient) CreateEditRawRequest(eReq EditReq) (EditResp, error) {
+	reqBody, err := json.Marshal(eReq)
+	if err != nil {
+		return EditResp{}, err
+	}
+	req, err := http.NewRequest("POST", c.ApiBase+"/completions", bytes.NewBuffer(reqBody))
+	if err != nil {
+		return EditResp{}, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+c.Token)
+	resp, err := c.Client.Do(req)
+	if err != nil {
+		return EditResp{}, err
+	}
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return EditResp{}, err
+	}
+	eResp := EditResp{}
+	err = json.Unmarshal(respBody, &eResp)
+	return eResp, err
 }
